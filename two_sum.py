f58b403b7ca06038aaf63b8494f4229e7e4bc0e0
Khushi Sinha <60260300+khushisinhax@users.noreply.github.com>
2024-04-08 17:16:34 +0800

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -8,4 +8,9 @@ class Solution:
             num_dict[num] = i
         return []
 
+""1.Create an empty dictionary num_dict to store the numbers and their indices.
+2.iterate through the nums array.
+3.For each number,  calculate the complement, which is the number that needs to be added to the current number to get the target.
+4.check if the complement is already in the num_dict. If it is,  return the indices of the two numbers that add up to the target.
+5the complement is not in the num_dict, add the current number and its index to the dictionary.""
 
